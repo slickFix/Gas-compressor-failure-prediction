6facf6b3d8b8ed88b7e81c403a1f6b4e487276c0
slickFix <sidd.200894@gmail.com>
2019-01-23 15:55:49 +0530

    changing the hidden layer dimension (fixing typos)

--- gas_compressor.py
+++ gas_compressor.py
@@ -134,10 +134,10 @@ b4 = tf.Variable(tf.zeros(num_outputs),name='b4')
 
 # layers description
 
-hid_layer1 = act_func(tf.add(tf.matmul(X_ph  , w1),b1))
-hid_layer2 = act_func(tf.add(tf.matmul(X_ph  , w1),b1))
-hid_layer3 = act_func(tf.add(tf.matmul(X_ph  , w1),b1))
-output_layer = tf.add(tf.matmul(X_ph  , w1),b1)
+hid_layer1 = act_func(tf.add(tf.matmul(X_ph, w1),b1))
+hid_layer2 = act_func(tf.add(tf.matmul(hid_layer1, w2),b2))
+hid_layer3 = act_func(tf.add(tf.matmul(hid_layer2, w3),b3))
+output_layer = tf.add(tf.matmul(hid_layer3, w4),b4)
 
 
 # loss function
