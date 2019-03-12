e781510bdfe0d57040ae2eb4fba94c8188e8dcd0
slickFix <sidd.200894@gmail.com>
2019-03-12 16:05:32 +0530

    completed training of all AE layers

--- gas_compressor_sparseAE.py
+++ gas_compressor_sparseAE.py
@@ -85,6 +85,73 @@ def initialise_parameter(n_x,n_y):
             'B_AE':B_AE}
     return parameters
 
+def initialise_parameter_ae_all(n_x,n_y):
+    ''' all layers of the FC is trained by ae '''
+    # defining parameters
+    input_feat = n_x
+    hidden1_nodes = 50
+    hidden2_nodes = 100
+    hidden3_nodes = 50
+    output_feat = n_y
+    
+    # defining variable initializer
+    var_init = tf.variance_scaling_initializer()
+    
+    # defining weights and bias
+    W1 = tf.Variable(var_init([input_feat,hidden1_nodes]),dtype=tf.float32,name='w1')
+    W2 = tf.Variable(var_init([hidden1_nodes,hidden2_nodes]),dtype=tf.float32,name='w2')
+    W3 = tf.Variable(var_init([hidden2_nodes,hidden3_nodes]),dtype=tf.float32,name='w3')
+    W4 = tf.Variable(var_init([hidden3_nodes,output_feat]),dtype=tf.float32,name='w4')
+    
+    W_AE = tf.Variable(var_init([hidden3_nodes,input_feat]),dtype=tf.float32,name='w_ae')
+    
+    b1 = tf.Variable(tf.zeros(hidden1_nodes),name='b1')
+    b2 = tf.Variable(tf.zeros(hidden2_nodes),name='b2')
+    b3 = tf.Variable(tf.zeros(hidden3_nodes),name='b3')
+    b4 = tf.Variable(tf.zeros(output_feat),name='b4')
+    
+    B_AE = tf.Variable(tf.zeros(input_feat),name='b_ae')
+
+    parameters = {            
+            'W1':W1,
+            'W2':W2,
+            'W3':W3,
+            'W4':W4,
+            'W_AE':W_AE,
+            'b1':b1,
+            'b2':b2,
+            'b3':b3,
+            'b4':b4,
+            'B_AE':B_AE}
+    return parameters
+
+def fwd_propagation_ae_all(x_ph,parameters):
+    ''' all layers of the FC is trained by ae '''
+    
+    W1 = parameters['W1']
+    W2 = parameters['W2'] 
+    W3 = parameters['W3'] 
+    W4 = parameters['W4']  
+    W_AE = parameters['W_AE'] 
+    b1 = parameters['b1']  
+    b2 = parameters['b2']  
+    b3 = parameters['b3']  
+    b4 = parameters['b4'] 
+    B_AE = parameters['B_AE'] 
+    
+    act_fn = tf.nn.relu
+    
+    hid_layer1 = act_fn(tf.add(tf.matmul(x_ph,W1),b1))
+    hid_layer2 = act_fn(tf.add(tf.matmul(hid_layer1,W2),b2))
+    hid_layer3 = act_fn(tf.add(tf.matmul(hid_layer2,W3),b3))
+    
+    output_layer = tf.add(tf.matmul(hid_layer3,W4),b4)
+    
+    x_hat = tf.nn.sigmoid(tf.add(tf.matmul(hid_layer1,W_AE),B_AE))
+    
+    return output_layer,x_hat,hid_layer1,hid_layer2,hid_layer3
+
+
 def fwd_propagation(x_ph,parameters):
     
     W1 = parameters['W1']
@@ -109,7 +176,7 @@ def fwd_propagation(x_ph,parameters):
     x_hat = tf.nn.sigmoid(tf.add(tf.matmul(hid_layer1,W_AE),B_AE))
     
     return output_layer,x_hat,hid_layer1
-    
+
 def compute_cost_fc(logits,y_ph,reg_lambda):
     
     loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=y_ph)
@@ -124,6 +191,29 @@ def compute_cost_fc(logits,y_ph,reg_lambda):
 def kl_divergence(p,p_hat):
     return p*tf.log(p)-p*tf.log(p_hat)+(1-p)*tf.log(1-p)-(1-p)*tf.log(1-p_hat)
 
+def compute_cost_ae_all(x_hat,x_ph,parameters,reg_lambda,hid_layer1,hid_layer2,hid_layer3,rho,beta):
+    
+    diff = x_hat-x_ph
+    
+    p_hat1 = tf.reduce_mean(tf.clip_by_value(hid_layer1,1e-10,1.0,name='clipper'),axis = 0)
+    p_hat2 = tf.reduce_mean(tf.clip_by_value(hid_layer2,1e-10,1.0,name='clipper'),axis = 0)    
+    p_hat3 = tf.reduce_mean(tf.clip_by_value(hid_layer3,1e-10,1.0,name='clipper'),axis = 0)
+    
+    kl1 = kl_divergence(rho,p_hat1)
+    kl2 = kl_divergence(rho,p_hat2)
+    kl3 = kl_divergence(rho,p_hat3)
+    
+    kl_loss = beta*(tf.reduce_sum(kl1)+tf.reduce_sum(kl2)+tf.reduce_sum(kl3))
+    
+    W1 = parameters['W1']
+    W2 = parameters['W2'] 
+    W3 = parameters['W3'] 
+    W_AE = parameters['W_AE']
+    l2_loss = reg_lambda*(tf.nn.l2_loss(W1)+tf.nn.l2_loss(W2)+tf.nn.l2_loss(W3)+tf.nn.l2_loss(W_AE))
+    
+    loss = tf.reduce_mean(tf.reduce_sum(diff**2,axis=1))+kl_loss+l2_loss
+    
+    return loss
 
 def compute_cost_ae(x_hat,x_ph,parameters,reg_lambda,hid_layer1,rho,beta):
     
@@ -170,10 +260,12 @@ def model(X_train_scaled_s,y_train_s,X_test_s,y_test_s,sc,learning_rate = 1e-3,r
     x_ph,y_ph = create_placeholder(n_x,n_y)
     
     # parameters initialisation
-    parameters = initialise_parameter(n_x,n_y)
+    #parameters = initialise_parameter(n_x,n_y)
+    parameters = initialise_parameter_ae_all(n_x,n_y)
     
     # forward propagation
-    logits,x_hat,hid_layer1 = fwd_propagation(x_ph,parameters)
+    #logits,x_hat,hid_layer1 = fwd_propagation(x_ph,parameters)
+    logits,x_hat,hid_layer1,hid_layer2,hid_layer3 = fwd_propagation_ae_all(x_ph,parameters)
     
     # cost history saving
     cost_fc_li = []
@@ -181,13 +273,15 @@ def model(X_train_scaled_s,y_train_s,X_test_s,y_test_s,sc,learning_rate = 1e-3,r
     
     # cost calculation
     cost_fc = compute_cost_fc(logits,y_ph,reg_lambda)
-    cost_ae = compute_cost_ae(x_hat,x_ph,parameters,reg_lambda,hid_layer1,rho,beta)
+    #cost_ae = compute_cost_ae(x_hat,x_ph,parameters,reg_lambda,hid_layer1,rho,beta)
+    cost_ae = compute_cost_ae_all(x_hat,x_ph,parameters,reg_lambda,hid_layer1,hid_layer2,hid_layer3,rho,beta)
     
     # optimizers
     optimizer_fc = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost_fc)
     optimizer_ae = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost_ae)
     
     init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
     
     with tf.Session() as sess:
         sess.run(init)
@@ -228,8 +322,19 @@ def model(X_train_scaled_s,y_train_s,X_test_s,y_test_s,sc,learning_rate = 1e-3,r
                 epoch_cost += c_fc/n_batches
             
             cost_fc_li.append(epoch_cost)
+            
             if (epoch+1)%2 == 0:
                 print("Epoch: ",epoch+1," cost: ",epoch_cost)
+                
+            if epoch == 0:
+                past_training_loss = epoch_cost
+                continue
+        
+            elif epoch_cost < past_training_loss:# if new loss is less than past loss "save new model parameters"            
+                saver.save(sess, "./model_ae_fc_all/gas_compressor.ckpt")
+                print(f'saving model for epoch : {epoch}')
+                past_training_loss = epoch_cost
+            
         
         correct_pred = tf.equal(tf.math.argmax(logits,axis=1),tf.math.argmax(y_ph,axis=1))
         acc = tf.reduce_mean(tf.cast(correct_pred,'float'))
@@ -241,7 +346,7 @@ def model(X_train_scaled_s,y_train_s,X_test_s,y_test_s,sc,learning_rate = 1e-3,r
         print("Training accuracy of the AE_FC model is : ",acc.eval(feed_train))
         
         feed_test = {}
-        X_test_scaled_s = sc.transform(X_test_s)
+        X_test_scaled_s = sc.transform(X_test_s)  
         feed_test[x_ph] = X_test_scaled_s.astype(np.float32)
         feed_test[y_ph] = y_test_s.astype(np.float32)
         
@@ -273,11 +378,11 @@ if __name__ == '__main__':
     sc = StandardScaler().fit(X_train_s)
     X_train_scaled_s = sc.transform(X_train_s)
     
-    
+    # training model
     model(X_train_scaled_s,y_train_s,X_test_s,y_test_s,sc,learning_rate = 0.001,n_epochs = 50,batch_size=100)
-
     
-
+    # model evaluation on 40 % not seen dataset
+    
 
 
 
